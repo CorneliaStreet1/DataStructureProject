9e058d030810534494003a65fea8ca93e073ea0a
cottonb <472186159@qq.com>
2022-04-01 09:13:05 +0800

    尝试分包

---
+++ src/main/java/HYH/ExamSearch.java
@@ -0,0 +1,8 @@
+package HYH;
+
+import HYH.Model.System_models;
+public class ExamSearch extends System_models{
+    public ExamSearch(String s) {
+        super(s);
+    }
+}
--- src/main/java/HYH/Model/System_close.java
+++ src/main/java/HYH/Model/System_close.java
@@ -1,4 +1,4 @@
-package HYH;
+package HYH.Model;
 
 public class System_close extends System_model{
     public System_close(String s) {
