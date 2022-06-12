26fbc2a672f95198efce0cdc7b11ef0c48c45652
cottonb <472186159@qq.com>
2022-06-12 22:22:11 +0800

    activity

--- src/main/java/HYH/Manager/System_manager.java
+++ src/main/java/HYH/Manager/System_manager.java
@@ -2,14 +2,18 @@ package HYH.Manager;
 
 import HYH.Model.*;
 import HYH.Manager.CourseModify.*;
+import HYH.ActivityManager.*;
 
 public class System_manager extends System_models{
     CourseModify courseModify;
+    ActivityManager activityManager;
 
     public System_manager(String s) {
         super(s);
         courseModify=new CourseModify("修改课程");
+        activityManager=new ActivityManager("修改活动");
         add_model("1",courseModify);
+        add_model("2",activityManager);
     }
 
 }
